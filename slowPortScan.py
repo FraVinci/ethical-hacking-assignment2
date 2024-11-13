668f70808cba22c897bc98bd673adaa4e70790d9
FraVinci <34674317+FraVinci@users.noreply.github.com>
2024-11-13 15:41:23 +0100

    Update slowPortScan.py

--- slowPortScan.py
+++ slowPortScan.py
@@ -1,41 +1,26 @@
-from scapy.all import *
-import random
 import time
-
-def slow_port_scan(target_ip, ports):
-  
-    print(f"Start slow port scan on {target_ip}...\n")
-    
-    #Mixing the order of ports to avoid sequential scanning
-    random.shuffle(ports)
-
-    for port in ports:
-
-        packet = IP(dst=target_ip) / TCP(dport=port, flags="S")
-        
-        #Send the package and wait for a reply
-        response = sr1(packet, timeout=1, verbose=0)
-        
-        if response is not None:
-            if response.haslayer(TCP) and response[TCP].flags == 0x12:
-                print(f"Porta {port} open")
-                #Send the RST packet to close the connection
-                sr(IP(dst=target_ip) / TCP(dport=port, flags="R"), timeout=1, verbose=0)
-            elif response.haslayer(TCP) and response[TCP].flags == 0x14:
-                print(f"Porta {port} closed")
-            elif response.haslayer(ICMP):
-                print(f"Filtered {port} port (received ICMP packet)")
-        else:
-            print(f"Port {port} no response")
-        
-        #Random delay between requests to avoid detection
-        delay = random.uniform(1, 30)
-        time.sleep(delay)
-
-    print("\nScan completed")
+import random
+from scapy.all import *
 
 #IP address of the victim's machine
-target_ip = "192.168.56.102" 
+destIp = "192.168.56.102"
+#Port to scan, random order
+portsToScan = list(range(20, 1024))
+random.shuffle(portsToScan)
 
-ports = list(range(20, 30))  #List of scanning ports
-slow_port_scan(target_ip, ports)
+def fragmentedSlowScan(destIp, portsToScan, minDelay=60, maxDelay=300):
+    for port in portsToScan:
+        #SYN packet with random ID
+        pkt = IP(dst=destIp, ttl=random.randint(64, 128), id=random.randint(1000, 50000), flags="MF") / TCP(dport=port, flags="S")
+
+        #First packet fragment
+        send(pkt, verbose=0)
+        
+        #Random delay
+        delay = random.uniform(minDelay, maxDelay)
+        print(f"Fragment packet sent to port {port}\nNext scan: {delay/60:.2f} minutes\n-------------")
+        
+        time.sleep(delay)
+
+print(f"start slow port scan on {destIp}...\n")
+fragmentedSlowScan(destIp, portsToScan)
